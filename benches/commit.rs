166f754a63573e729ec3b108a36abd55845f444a
Sebastien La Duca <sladuca777@gmail.com>
2021-09-03 17:31:59 -0400

    publish 0.1.6

--- benches/commit.rs
+++ benches/commit.rs
@@ -18,7 +18,7 @@ fn bench_commit<E: Engine, const NUM_COEFFS: usize>(c: &mut Criterion) {
     for i in 0..NUM_COEFFS {
         coeffs[i] = rng.gen::<u64>().into();
     }
-    let polynomial = Polynomial::new_from_coeffs(coeffs, NUM_COEFFS - 1);
+    let polynomial = Polynomial::new(coeffs);
 
     c.bench_function(
         format!("bench_commit, degree {}", NUM_COEFFS - 1).as_str(),
